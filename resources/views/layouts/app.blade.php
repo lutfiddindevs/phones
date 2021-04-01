aaefa3ca3f51937a4f93b92153a8d77ff807c403
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-01 22:20:51 +0500

    application layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>phones</title>
+	<meta charset="UTF-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gradient-to-r from-gray-100 to-gray-200">
+	@yield('content')
+</body>
+</html>
