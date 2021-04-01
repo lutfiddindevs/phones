64cae09ffa9d644a2ebbd88a47f4a5d9124e97ca
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-01 23:02:07 +0500

    making phone lists dynamic

--- resources/views/phones/index.blade.php
+++ resources/views/phones/index.blade.php
@@ -7,18 +7,20 @@
 		</div>
 	
 		<div class="w-5/6 py-10">
+			@foreach($phones as $phone)
 			<div class="m-auto">
 				<span class="uppercase text-blue-500 font-bold text-xs italic">
-					Founded: 2020 
+					Founded: {{ $phone->founded }} 
 				</span>
 				<h2 class="text-gray-700 text-5xl">
-					Samsung Galaxy A11
+					{{ $phone->name }}
 				</h2>
 				<p class="text-lg text-gray-700 py-6">
-					The Samsung Galaxy A11 is an entry-level Android smartphone developed by Samsung Electronics. It was first announced on March 13, 2020, as a successor to the Samsung Galaxy A10. A similar device, Galaxy M11, was first released on March 2020, but with a larger battery capacity (5000 mAh)
+					{{ $phone->description }}
 				</p>
 				<hr class="mt-4 mb-8">
 			</div>
+			@endforeach
 		</div>
 	</div>
 @endsection
