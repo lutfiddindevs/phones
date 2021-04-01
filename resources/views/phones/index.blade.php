bf12b80071ac45193bd4de6f6b7bf383ebca3eac
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-01 22:21:28 +0500

    index page listed phones

---
+++ resources/views/phones/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="m-auto w-4/5 py-24">
+		<div class="text-center">
+			<h1 class="text-5xl uppercase bold">Phones</h1>
+		</div>
+	
+		<div class="w-5/6 py-10">
+			<div class="m-auto">
+				<span class="uppercase text-blue-500 font-bold text-xs italic">
+					Founded: 2020 
+				</span>
+				<h2 class="text-gray-700 text-5xl">
+					Samsung Galaxy A11
+				</h2>
+				<p class="text-lg text-gray-700 py-6">
+					The Samsung Galaxy A11 is an entry-level Android smartphone developed by Samsung Electronics. It was first announced on March 13, 2020, as a successor to the Samsung Galaxy A10. A similar device, Galaxy M11, was first released on March 2020, but with a larger battery capacity (5000 mAh)
+				</p>
+				<hr class="mt-4 mb-8">
+			</div>
+		</div>
+	</div>
+@endsection
