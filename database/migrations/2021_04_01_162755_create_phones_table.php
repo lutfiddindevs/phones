f2081218b4d4ef8d7b3717e9fb52feeccf8a1f90
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-01 22:23:04 +0500

    phones table migration

---
+++ database/migrations/2021_04_01_162755_create_phones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('founded');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
