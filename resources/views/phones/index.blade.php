a7a926396b937d9b05dfcce27cabd26833f7274e
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-02 15:20:33 +0500

    added delete phone link

--- resources/views/phones/index.blade.php
+++ resources/views/phones/index.blade.php
@@ -5,6 +5,21 @@
 		<div class="text-center">
 			<h1 class="text-5xl uppercase bold">Phones</h1>
 		</div>
+		@if(Session::has('d_message'))
+           <div class="alert pt-3 pb-3 bg-green-500 mt-4">
+           	   {{ Session::get('d_message') }}
+           </div>
+		@endif
+		@if(Session::has('e_message'))
+           <div class="alert pt-3 pb-3 bg-green-500 mt-4">
+           	   {{ Session::get('e_message') }}
+           </div>
+		@endif
+		@if(Session::has('c_message'))
+           <div class="alert pt-3 pb-3 bg-green-500 mt-4">
+           	   {{ Session::get('c_message') }}
+           </div>
+		@endif
 	    <div class="pt-10">
 	    	<a href="phones/create" class="border-b-2 pb-2 border-dotted italic text-gray-500">
 	    		 Add a new Phone &rarr;
@@ -14,7 +29,14 @@
 			@foreach($phones as $phone)
 			<div class="m-auto">
 				<div class="float-right">
-					<a href="{{ $phone->id }}/edit" class="border-b-2 pb-2 border-dotted italic text-green-500">Edit &rarr; </a>
+					<a href="phones/{{ $phone->id }}/edit" class="border-b-2 pb-2 border-dotted italic text-green-500">Edit &rarr; </a>
+					<form action="phones/{{ $phone->id }}" method="post" class="pt-3"> 
+						@csrf
+						@method('delete')
+						<button type="submit" class="border-b-2 pb-2 border-dotted italic text-red-500">
+							Delete &rarr;
+						</button>
+					</form>
 				</div>
 				<span class="uppercase text-blue-500 font-bold text-xs italic">
 					Founded: {{ $phone->founded }} 
